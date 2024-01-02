892b397a29db64a9e736ca150f12b8897defcb96
kiritoEM <johankirito64@gmail.com>
2024-01-02 07:57:47 +0100

    add app configuration

---
+++ src/server/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+const compression = require("compression");
+const dotenv = require("dotenv").config();
+
+// App middlewares
+app.use(cors());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.get("/home", (req, res) => {
+  res.status(200).json("Welcome, your app is working well");
+});
+
+// App routes
+// app.use("/auth", require("./../routes/Auth.routes"));
+// app.use("/chat", require("./../routes/chat.routes"));
+
+module.exports = app;
